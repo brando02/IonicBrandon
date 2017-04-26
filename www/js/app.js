681ee765ef93299a3447a9b61c592cbed9d38d0b
Brandon Colina <bl.empresas@gmail.com>
2017-04-25 20:29:04 -0400

    Finally Changes

---
+++ www/js/app.js
@@ -0,0 +1,16 @@
+angular.module('ionicBrandonColina', ['ionic'])
+
+.controller('ionicBrandonColinaCtrl', function($scope) {
+
+
+    $scope.helloWorld = "Hola Mundo";
+
+    $scope.content = [
+        {
+          Description:'Soy Brandon Colina Ingeniero de Sistemas'
+        },
+        {
+          Description:'Especialista en el desarrollo de aplicaciones'
+        }
+    ];
+});
